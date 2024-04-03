8cdc2372df83c0f0f07ade400f6ef6b5b89e25ab
Vishal <92736996+vishaljaiswal890@users.noreply.github.com>
2024-04-03 17:16:56 +0530

    Fixed few bugs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,7 +5,6 @@ import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="App">
